e474e30eb5229a9ceeabc618481108f2819f1137
artemhiz <92753758+artemhiz@users.noreply.github.com>
2022-01-16 19:31:53 +0300

    Добавлено ПЗ №10

--- PZ_10/PZ_10_2.py
+++ PZ_10/PZ_10_2.py
@@ -4,6 +4,11 @@ f1 = open('text18-29.txt', 'r')
 a = f1.read()
 print(a)
 a = a.split('\n')
+b = 0
+for i in a:
+    b += len(i)
+print('Кол-во символов в тексте: ' + str(b))
+
 a.insert(2, a.pop(-1))
 
 f2 = open('FfPZ_10_2', 'w')
