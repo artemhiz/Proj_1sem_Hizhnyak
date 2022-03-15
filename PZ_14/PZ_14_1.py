ef3f44e2f530321a3c3d69fddf37cc402cb771b0
artemhiz <artemhiz@icloud.com>
2022-03-15 15:14:51 +0300

    Добавлено ПЗ-14

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,13 +1,21 @@
+# Программа находит даты, записанные в существующих форматах (ДД.ММ.ГГГГ или ДД/ММ/ГГГГ), считает их количество и
+# записывает в новый файл в формате ДД/ММ/ГГГГ
 import re
 
-slashdatefind = re.compile(r'\n([0-9]{2}/02/[0-9]{2})', re.S)
-dotdatefind = re.compile(r'\n([0-9]{2}\.02\.[0-9]{2})', re.S)
+slashfind = re.compile(r'\n([0-9]{2}/02/[0-9]{2})', re.S)
+dotfind = re.compile(r'\n([0-9]{2}\.02\.[0-9]{2})', re.S)
+
 with open('dates', 'r') as ori:
     f = ori.read()
-    slashdates = slashdatefind.findall(f)
+    slashdates = slashfind.findall(f)
     print('ДД/ММ/ГГГГ:', len(slashdates))
-    dotdates = dotdatefind.findall(f)
+    dotdates = dotfind.findall(f)
     print('ДД.ММ.ГГГГ:', len(dotdates))
 
-    with open('result', 'w') as destination:
-        pass
+slashdates.extend([re.sub('\.', '/', dotdates.pop(0)) for i in range(len(dotdates))])
+
+with open('result', 'w') as destination:
+    destination.write('\n'.join(slashdates))
+print('Даты перенесены в файл "result". Содержимое файла ниже:')
+with open('result', 'r') as check:
+    print(re.sub('\n', ', ', check.read()))
