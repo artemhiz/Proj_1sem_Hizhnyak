8aa100004a7561958b57339f942b80ac879ccceb
Student <artemhiz@icloud.com>
2022-03-26 16:29:40 +0300

    Добавлено ПЗ-15

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -1,6 +1,8 @@
 # Программа меняет все элементы последней строки матрицы на 0
 from random import randint as random
-l = [[random(0, 9) for i in range(0, 20)] for e in range(0, random(2, 4))]
-print("Изначальная матрица: ", l)
+import re
+l = [[random(0, 9) for i in range(0, 5)] for e in range(0, random(2, 4))]
+print("Изначальная матрица: ", re.sub(',', '', re.sub('( \[)|(\]\])', '', re.sub('(\[\[)|(\],)', '\n', str(l)))))
 l[-1] = [0 for s in l[-1]]
-print("Итоговая матрица:    ", l)
+result = re.sub(',', '', re.sub('( \[)|(\]\])', '', re.sub('(\[\[)|(\],)', '\n', str(l))))
+print("\nИтоговая матрица:    ", result)
--- PZ_15/PZ_15_2.py
+++ PZ_15/PZ_15_2.py
@@ -1,6 +1,15 @@
+# Программа удваивает значение столбца, порядковый номер которого вводится с клавиатуры
+# ПРИМЕЧАНИЕ: счёт столбцов начинается с 1
 from random import randint as random
-a = [[random(0, 9) for i in range(0, 20)] for e in range(0, random(2, 4))]
-print("Изначальная матрица: ", a)
-n = int(input("Введите номер столбца: "))
-a[(e for e in range(len(a)))][(n - 1)] *= 2
-print("Итоговая матрица:    ", a)
+import re
+a = [[random(0, 1) for i in range(0, 5)] for e in range(0, random(2, 4))]
+print("Изначальная матрица: ", re.sub(',', '', re.sub('( \[)|(\]\])', '', re.sub('(\[\[)|(\],)', '\n', str(a)))))
+
+while True:
+    n = int(input('\nВведите порядковый номер столбца: ')) - 1
+    if n <= 0:
+        print('ВВедите значение больше ноля')
+    else: break
+for i in range(len(a)):
+    a[i][n] *= 2
+print("\nИтоговая матрица:    ", re.sub(',', '', re.sub('( \[)|(\]\])', '', re.sub('(\[\[)|(\],)', '\n', str(a)))))
